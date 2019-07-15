5d993c379f4ff437527676ab41208c6ac592d0e5
Naim Buru <nbur4u@gmail.com>
2019-07-15 15:12:04 +0300

    add material-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,35 @@
-import App, { Container } from 'next/app'
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
+import React from "react";
+import App, { Container } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/theme";
 
-const theme = {
-  colors: {
-    primary: '#0070f3'
+class MyApp extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
   }
-}
 
-export default class MyApp extends App {
-  render () {
-    const { Component, pageProps } = this.props
+  render() {
+    const { Component, pageProps } = this.props;
+
     return (
       <Container>
+        <Head>
+          <title>My page</title>
+        </Head>
         <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
           <Component {...pageProps} />
         </ThemeProvider>
       </Container>
-    )
+    );
   }
 }
+
+export default MyApp;

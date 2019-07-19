911fe630b7a6bb0456900cf5b6ad9a30dcb1e616
Naim Buru <nbur4u@gmail.com>
2019-07-19 12:25:41 +0300

    add some meta tags for seo

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,23 @@ class MyApp extends App {
     return (
       <Container>
         <Head>
-          <title>My page</title>
+          <title key="title">Naim Buru</title>
+          <meta
+            key="description"
+            name="description"
+            content="Personal website of a full stack developer Naim Buru"
+          />
+
+          <meta key="og:url" property="og:url" content="https://naimburu.com" />
+          <meta key="og:type" property="og:type" content="website" />
+          <meta key="og:title" property="og:title" content="Naim Buru" />
+          <meta
+            key="og:description"
+            property="og:description"
+            content="Personal website of a full stack developer Naim Buru"
+          />
+
+          <meta key="og:locale" property="og:locale" content="en_US" />
         </Head>
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
